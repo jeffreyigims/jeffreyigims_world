e6b9a73d411839086eff0149bbbee2435dbccd19
Andy <Achen725.cfl@gmail.com>
2021-03-08 12:17:59 -0500

    Added controller and views as specified - user tested

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,10 @@
 class Article < ApplicationRecord
-    has_many :categories
+    has_one :category
 
     validates_presence_of :title
     validates_presence_of :content
     
     scope :alphabetical, -> { order('title') }
-    scope :active,              -> { where('active = ?', true) }
+    scope :active, -> { where('active = ?', true) }
 
 end
